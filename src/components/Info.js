69b4c20154275790bf6e9c48e00c8aa3db38d61d
Christopher Bowers <c_bowers@me.com>
2022-03-22 10:43:10 -0400

    Responsive styles

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,13 @@
 export default function Info() {
   return (
     <div className="flex-wrapper">
-      <div className="image">
+      <div className="image overlay">
         <img src="https://images.unsplash.com/photo-1524633712235-22da046738b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80" alt="Supreme Court" />
       </div>
       <div className="text">
         <h2>Lorem ipsum dolor sit amet consectetur.</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias labore numquam alias dolorem assumenda in incidunt veritatis recusandae voluptas fuga vel inventore rerum quaerat, dolor dolores, officiis cupiditate.</p>
-        <a href="#" className="button">This is a Button</a>
+        <button className="button">This is a Button</button>
       </div>
     </div>
   )
